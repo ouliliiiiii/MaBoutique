3726296047c0dc03108ddb0dfac186b9b32013e7
unknown <anonymeus10@gmail.com>
2024-02-26 20:16:30 +0100

    update

---
+++ database/migrations/2024_02_25_150129_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('comment')->nullable(false);
+            $table->unsignedBigInteger('produit_id')->indexe();
+            $table->unsignedBigInteger('user_id')->indexe();
+            $table->timestamps();
+
+            $table->foreign('produit_id')->references('id')->on('produits')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
